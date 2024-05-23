a3ce7f5831f4d853c4fd287464f16d10e97969d3
diongzy <dion.zy627@gmail.com>
2024-05-23 13:57:03 +0800

    Update

--- IPEX-LLM/docker/llm/finetune/qlora/cpu/docker/start-qlora-finetuning-on-cpu.sh
+++ IPEX-LLM/docker/llm/finetune/qlora/cpu/docker/start-qlora-finetuning-on-cpu.sh
@@ -34,7 +34,7 @@ then
      -genv KMP_AFFINITY="granularity=fine,none" \
      -genv KMP_BLOCKTIME=1 \
      -genv TF_ENABLE_ONEDNN_OPTS=1 \
-     python qlora_finetuning_cpu.py$MODEL_PARAM $DATA_PARAM
+     python qlora_finetuning_cpu.py $MODEL_PARAM $DATA_PARAM
 else
   python qlora_finetuning_cpu.py $MODEL_PARAM $DATA_PARAM
 fi
