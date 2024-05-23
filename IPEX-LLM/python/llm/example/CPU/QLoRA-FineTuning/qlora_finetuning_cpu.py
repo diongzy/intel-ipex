f74e2e1e407d47ba2f8b682e08295ce2963ac8f9
diongzy <dion.zy627@gmail.com>
2024-05-23 10:32:18 +0800

    Update qlora_finetuning_cpu.py
    load custom dataset

--- IPEX-LLM/python/llm/example/CPU/QLoRA-FineTuning/qlora_finetuning_cpu.py
+++ IPEX-LLM/python/llm/example/CPU/QLoRA-FineTuning/qlora_finetuning_cpu.py
@@ -101,6 +101,7 @@ if __name__ == "__main__":
         model=model,
         train_dataset=dataset["train"],
         eval_dataset= dataset["val"],
+        dataset_text_field="prompt",
         args=transformers.TrainingArguments(
             per_device_train_batch_size=16,
             gradient_accumulation_steps=2,
