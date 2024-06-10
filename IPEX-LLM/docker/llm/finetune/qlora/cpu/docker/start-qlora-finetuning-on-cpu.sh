cb042591134087f175692fd5b35ac8aac8d57ad4
diongzy <dion.zy627@gmail.com>
2024-06-10 17:25:29 +0800

    without tsm

--- IPEX-LLM/docker/llm/finetune/qlora/cpu/docker/start-qlora-finetuning-on-cpu.sh
+++ IPEX-LLM/docker/llm/finetune/qlora/cpu/docker/start-qlora-finetuning-on-cpu.sh
@@ -8,9 +8,9 @@ export CPU_CORES=$(nproc)
 source ipex-llm-init -t
 export WANDB_API_KEY='2bd9da9f8c9031d1a7bdddb45f3bdf84f3139346'
 ##For AMX 
-export ONEDNN_MAX_CPU_ISA=AVX512_CORE_AMX 
+# export ONEDNN_MAX_CPU_ISA=AVX512_CORE_AMX 
 # ## For Jemmaloc 
-# export ONEDNN_MAX_CPU_ISA=AVX512_CORE_VNNI
+export ONEDNN_MAX_CPU_ISA=AVX512_CORE_VNNI
 ## For Jemmaloc or Jem + AMX
 export LD_PRELOAD=/usr/local/lib/libjemalloc.so
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:60000,muzzy_decay_ms:-1"
